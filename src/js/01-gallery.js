2b199abe18fa0df61f816a49891837f522a14344
spqrvalerii <1982elcondorpasa@gmail.com>
2023-01-27 13:02:01 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,26 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = galleryItems
+  .map(item => {
+    return `<a class="gallery__item" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>`
+  })
+  .join('');
+
+galleryEl.innerHTML = galleryMarkup;
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
