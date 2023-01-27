f71263986f18e2a69bbf9587ed792613730a5c74
spqrvalerii <1982elcondorpasa@gmail.com>
2023-01-28 01:01:53 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,37 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const email = document.querySelector('input[name="email"]');
-const message = document.querySelector('textarea[name="message"]');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-const localStorageObject = {};
 
 form.addEventListener('input', throttle(onInputForm, 500));
-form.addEventListener('submit', onSubmitForm);
 
 function onInputForm(e) {
-  let target = e.target;
-  localStorageObject[target.name] = target.value;
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(localStorageObject));
-};
+  const dataObject = { email: email.value, message: message.value };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataObject));
+}; 
+
+form.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(e) {
-  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
   e.preventDefault();
-  e.currentTarget.reset();
+  console.log({ email: email.value, message: message.value });
+  form.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
 };
-
-function save(key, value) {
-  try {
-    const serializedData = JSON.stringify(value);
-    localStorage.setItem(key, serializedData);
-  } catch (err) {
-    console.error(err);
-  }
-}
 
 function load(key) {
   try {
     const serializedState = localStorage.getItem(key);
     return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
   }
-}
+};
 
 const localStorageData = load(LOCALSTORAGE_KEY);
 if (localStorageData) {
   email.value = localStorageData.email;
   message.value = localStorageData.message;
-}
+};
